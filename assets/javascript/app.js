9147faf0e7ba7675869b1cb9ebc6cdc1af477b91
kkopp <kristina.kopp@gmail.com>
2018-10-27 23:18:46 -0600

    added most code, submit button working, but seems to break code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,78 +1,83 @@
 $( document ).ready(function() {
 
-var topics = ["happy", "sad", "excited", "silly", "angry", "upset", "scared"];
-
-function makeButtons() {
-
-    $(".buttonContainer").empty();
+  var topics = ["frustrated", "happy", "sad", "excited", "silly", "angry", "upset", "scared", "grumpy", "irate", "sleepy", "funny", "elated"];
   
-    for (var i = 0; i < topics.length; i++) {
-
-      var emotion = $("<button>");
-      emotion.addClass("topic");
-      emotion.attr("data-name", topics[i]);
-      emotion.text(topics[i]);
-      $(".buttonContainer").append(emotion);
-    }
-  }
-
-
-$("#addTopic").on("click", function() {
-  var topic = $("#topic-input").val().trim();
-  topics.push(topic);
-  makeButtons()
-
-  return false;
-});
-
-function displayImg() {
-  var topic = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5TRw6UUxhHk9vmQzHs0hHTMMQ4REtBFV&q=" + topics + "&limit=10";
-
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-
-  .done(function(response) {
-    console.log(response.data);
-
-    var results = response.data;
-
-    for (i = 0; i < results.length; i++) {
-    var imgDiv = $("<div class=gifs>");
-    var showImg = $("<img>");
-        showImg.attr("src", results[i].images.fixed_height_still.url);
-        showImg.attr("data-still", results[i].images.fixed_height_still.url);
-        showImg.attr("data-state", "still");
-        showImg.addClass("gif");
-        showImg.attr("data-animate", results[i].fixed_height.url);
+  function makeButtons() {
+  
+      $(".buttonContainer").empty();
     
-    imgDiv.append(showImg);
-
-    $("#images").prepend(showImg);
+      for (var i = 0; i < topics.length; i++) {
+  
+        var emotion = $("<button>");
+        emotion.addClass("btn btn-outline-secondary");
+        emotion.attr("data-name", topics[i]);
+        emotion.text(topics[i]);
+        $(".buttonContainer").append(emotion);
+      }
     }
+  
+  makeButtons();
+  
+  $("#addTopic").on("click", function() {
+    event.preventDefault();
+    var topic = $("#topic-input").val().trim();
+    topics.push(topic);
+    makeButtons()
+    return false;
+  });
+  
 
+  $("button").on("click", function () {
+    //console.warn("we're here 0");
+    var buttonTopics = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5TRw6UUxhHk9vmQzHs0hHTMMQ4REtBFV&q=" + buttonTopics + "&limit=10";
+  
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .done(function(response) {
+      console.log(response.data);
+
+      var results = response.data;
+      $("#images").empty();
+  
+      for (i = 0; i < results.length; i++) {
+
+      var imgDiv = $("<div>");
+      var p = $("<p>").text("Rating: " + results[i].rating);
+      var emotionImg = $("<img>");
+
+        emotionImg.attr("src", results[i].images.original_still.url);
+        emotionImg.attr("data-still", results[i].images.original_still.url);
+        emotionImg.attr("data-animate", results[i].images.original.url);
+        emotionImg.attr("data-state", "still");
+        emotionImg.attr("class", "gif");
+        imgDiv.append(p);
+        imgDiv.append(emotionImg);
+        $("#images").append(imgDiv);
+      }
+  
+    });
+  
   });
 
-}
+	function changeState(){
+		var state = $(this).attr("data-state");
+		var animateImage = $(this).attr("data-animate");
+		var stillImage = $(this).attr("data-still");
 
-$(document).on('click', '.gif', function(){
-	var state = $(this).attr('data-state');
-		if ( state == 'still'){
-                $(this).attr('src', $(this).data('animate'));
-                $(this).attr('data-state', 'animate');
-            }else{
-                $(this).attr('src', $(this).data('still'));
-                $(this).attr('data-state', 'still');
-            };
+		if (state == "still") {
+			$(this).attr("src", animateImage);
+			$(this).attr("data-state", "animate");
+		}
+
+		else if (state == "animate") {
+			$(this).attr("src", stillImage);
+			$(this).attr("data-state", "still");
+		}
+	}
+
+	$(document).on("click", ".gif", changeState);
+
 });
-
-
-});
-
-displayGifs();
-$(document).on("click", ".show", displayImg);
-
-makeButtons();
